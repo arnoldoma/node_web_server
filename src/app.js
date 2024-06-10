4995419fd9ea1e82d55389b42395b54dca21b160
amap <arnoldomap@gmail.com>
2024-06-10 12:00:50 -0600

    Realizamos modificaciones para que funcione con importaciones y exportaciones de tipo ES6

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
-const { envs } = require('./config/env');
+// const { envs } = require('./config/env');
+import { envs } from './config/env.js';
 
-const {startServer} = require('./server/server')
+// const {startServer} = require('./server/server')
+import {startServer} from './server/server.js';
 
-const main = ()=>{
+export const main = ()=>{
         startServer({
             port: envs.PORT,
             public_path: envs.PUBLIC_PATH
